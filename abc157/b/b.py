3b58ad398ec17ec11b6c305b52b48a1d02a1093e
YUUSUKE <yuusuke.o.work.1018@gmail.com>
2022-11-11 23:26:21 +0900

    Pass ABC157

--- abc157/b/b.py
+++ abc157/b/b.py
@@ -1,29 +1,55 @@
 ## ビンゴカードの配列作成
-bingo = [list(map(int, input().split())) for i in range(3)]
-
-## 数値入力
-n = int(input())
-## 入力された数値の確認(Loop処理)
-for _ in range(n):
-    b = int(input())
-    for i in range(3):
-        for j in range(3):
-            if bingo[i][j] == b:
-                bingo[i][j] = -1
+dp = []
 
 for i in range(3):
-    if bingo[i][0] == bingo[i][1] == bingo[i][2] == -1:
-        print("Yes")
+    tmp = list(map(int, input().split()))
+    dp.append(tmp)
+n = int(input())
+lst = [[False]*3 for i in range(3)]
+ 
+for i in range(n):
+    b = int(input())
+    for j in range(3):
+        for k in range(3):
+            if dp[j][k]==b:
+                lst[j][k]=True
+ 
+for i in range(3):
+    if lst[i].count(True)==3:
+        print('Yes')
         exit()
-    elif bingo[0][i] == bingo[1][i] == bingo[2][i] == -1:
-        print("Yes")
+for i in range(3):
+    if lst[0][i]==True and lst[1][i] ==True and lst[2][i]:
+        print('Yes')
         exit()
-    elif bingo[0][0] == bingo[1][1] == bingo[2][2] == -1:
-        print("Yes")
+if lst[0][0]==True and lst[1][1]==True and lst[2][2] ==True or lst[0][2]==True and lst[1][1] == True and lst[2][0]==True:
+    print('Yes')
+    exit()
+print('No')
+dp = []
+
+for i in range(3):
+    tmp = list(map(int, input().split()))
+    dp.append(tmp)
+n = int(input())
+lst = [[False]*3 for i in range(3)]
+
+for i in range(n):
+    b = int(input())
+    for j in range(3):
+        for k in range(3):
+            if dp[j][k]==b:
+                lst[j][k]=True
+
+for i in range(3):
+    if lst[i].count(True)==3:
+        print('Yes')
         exit()
-    elif bingo[0][2] == bingo[1][1] == bingo[2][0] == -1:
-        print("Yes")
+for i in range(3):
+    if lst[0][i]==True and lst[1][i] ==True and lst[2][i]:
+        print('Yes')
         exit()
-    else:
-        print("No")
-        exit()
+if lst[0][0]==True and lst[1][1]==True and lst[2][2] ==True or lst[0][2]==True and lst[1][1] == True and lst[2][0]==True:
+    print('Yes')
+    exit()
+print('No')
